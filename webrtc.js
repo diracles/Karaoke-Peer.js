53531de150711959751e1ba48756f25d4416134b
ALEXANDRA_DIRACLES <A.DIRACLES@GMAIL.COM>
2013-11-04 12:28:53 -0500

    monday shawn

--- webrtc.js
+++ webrtc.js
@@ -26,9 +26,6 @@ function requestHandler(req, res) {
 // WebSockets work with the HTTP server
 var io = require('socket.io').listen(httpServer);
 
-
-var users = new Array();
-
 // Register a callback function to run when we have an individual connection
 // This is run for each individual user that connects
 io.sockets.on('connection', 
@@ -38,35 +35,19 @@ io.sockets.on('connection',
 		console.log("We have a new client: " + socket.id);
 		
 		socket.on('peer_id', function(data) {
+			console.log("Received: 'peer_id' " + data);
 
-            var newUser = true;
-			//.log("Received: 'peer_id' " + data);
 			// We can save this in the socket object if we like
 			socket.peer_id = data;
 			console.log("Saved: " + socket.peer_id);
 
 			// We can loop through these if we like
-			for (var i  = 0; i < users.length; i++) {
+			for (var i  = 0; i < io.sockets.clients().length; i++) {
 				console.log("loop: " + i + " " + io.sockets.clients()[i].peer_id);
-				if (io.sockets.clients.id = users[i].id) {
-					console.log("they are equal");
-					users[i].id = io.sockets.clents.id;
-					newUser = false;
-				}  else  {
-					console.log("not equal");
-				}
-			}
-
-
-			if(newUser) {
-				var user = { };
-				user.id = io.sockets.clients.id;
-				users.push(user);
 			}
 			
 			// Tell everyone my peer_id
-			//socket.broadcast.emit('peer_id',data);
-			socket.broadcast.emit('peer_id',users);
+			socket.broadcast.emit('peer_id',data);
 		});
 		
 		
